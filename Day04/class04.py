8282467127a827b5dc77da5ecc017d5a1137033f
dianakooo <31654740+dianakooo@users.noreply.github.com>
2017-11-09 12:00:15 +0300

    Add files via upload

---
+++ Day04/class04.py
@@ -0,0 +1,77 @@
+#nums=[]
+#for n in range(5):
+#    print(nums)
+#    print(n)
+#    nums.append(n)
+#print(nums)
+#print("Length:", len(nums))
+
+#text = "Hello. My name is kolbasa"
+#words = text.split() #делит предложение на слова, разделенные пробелом
+#print(words)
+#print(words[4])
+#print(words[1:])
+#for w in words[::2]:
+#    print(w)
+
+#nums=[[1, 2, 3]]*3
+#print(nums)
+#for row in nums:
+#    print(row)
+#    print('inner')
+#    for n in nums:
+#        print(row) #печатает в столбик
+
+#nums=[[1, 2, 3]]*3
+#for row in nums:
+#    print(row)
+#    print('inner')
+#    for n in row:
+#        print(n, end=' ')
+#    print()
+
+#w = "hello world!"
+#letters = []
+#for j in w:
+ #   letters.append(j)
+#print(letters)
+
+#nums = [1, 2, 3]
+#nums_str = str(nums[0])
+#for d in nums[1:]:
+#    nums_str = nums_str + ' ' + str(d)
+#print(nums_str)
+
+#nums = [1, 2, 3]
+#nums_str = ""
+#for d in nums:
+#    nums_str = nums_str + str(d) + ' '
+#print([nums_str])
+#nums_str = nums_str[:-1] #убирает пробел в конце
+#print([nums_str])
+
+#letters = list("hello")
+#letters.pop()
+#print(letters)
+
+#letters = list("hello world")
+#print(letters)
+#print(sorted(letters))
+
+#nums = [[1, 2, 3, 4]]*5
+#print(nums)
+
+#i do not know
+#what we are doing
+#and at this point
+#i am too afraid to ask
+#no like seriously
+
+
+
+
+
+
+
+
+
