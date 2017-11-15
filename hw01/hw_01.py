a7be4866c69a087a3f967c501db6aa5697234b39
dianakooo <31654740+dianakooo@users.noreply.github.com>
2017-11-15 19:57:12 +0300

    Update hw_01.py

--- hw01/hw_01.py
+++ hw01/hw_01.py
@@ -2,21 +2,21 @@
 
 print("Привет!")
 
-input_a=input("Введите число a: ")
-a=int(input_a)
+input_a = input("Введите число a: ")
+a = int(input_a)
 
-input_b=input("Введите число b: ")
-b=int(input_b)
+input_b = input("Введите число b: ")
+b = int(input_b)
 
-input_c=input("Введите число c: ")
-c=int(input_c)
+input_c = input("Введите число c: ")
+c = int(input_c)
 
-if a+b==c:
+if a + b == c:
     print("Сумма a и b равна c")
 else:
     print("Сумма a и b не равна c")
 
-if a/b==c:
+if a / b == c:
     print("Частное a и b равно c")
 else:
     print("Частное a и b не равно c")
