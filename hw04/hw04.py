71990075d311b4514fa047b3aebc3082ad3fdbbc
dianakooo <31654740+dianakooo@users.noreply.github.com>
2017-11-22 23:06:06 +0300

    Update hw04.py

--- hw04/hw04.py
+++ hw04/hw04.py
@@ -9,14 +9,14 @@ print(words)
 punct_num = 0
 words_num = 0
 
-for word in words:
-    words_num += 1
-    if word[-1] != "." and word[-1] != ",":# or word[-1] != "?" or word[-1] != "!" or word[-1] != ":" or word[-1] != ";" or word[-1] != ")" or word[-1] != "]" or word[-1] != "}" or word[-1] != "\""
-        punct_num += 1
-
-print("Слов в тексте: ", words_num)
-print("Слов, не оканчивающихся на точку или запятую: ", punct_num)
-
-percent = 100*punct_num/words_num
-
-print(percent,"%")
+if not text:
+    print('Файл пуст! Выберите другой.')
+else:
+    for word in words:
+        words_num += 1
+        if word[-1] != "." and word[-1] != ",":# or word[-1] != "?" or word[-1] != "!" or word[-1] != ":" or word[-1] != ";" or word[-1] != ")" or word[-1] != "]" or word[-1] != "}" or word[-1] != "\""
+            punct_num += 1
+    print("Слов в тексте: ", words_num)
+    print("Слов, не оканчивающихся на точку или запятую: ", punct_num)
+    percent = 100*punct_num/words_num
+    print(percent,"%")
