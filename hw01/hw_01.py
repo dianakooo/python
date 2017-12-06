b6768f859a6b0e3017d2cdc42124390899529e98
dianakooo <31654740+dianakooo@users.noreply.github.com>
2017-12-06 12:19:54 +0300

    Update hw_01.py

--- hw01/hw_01.py
+++ hw01/hw_01.py
@@ -16,9 +16,29 @@ if a + b == c:
 else:
     print("Сумма a и b не равна c")
 
+if a * b == c:
+  print("Произведение a и b равно c")
+else:
+  print("Произведение a и b не равно c")
+  
+if a % b == c:
+  print("a даёт остаток c при делении на b")
+else:
+  print("a не даёт остаток c при делении на b")
+
+if a * c + b == 0:
+  print("c является решением линейного уравнения ax + b = 0")
+else:
+  print("c не является решением линейного уравнения ax + b = 0")  
+  
 if a / b == c:
     print("Частное a и b равно c")
 else:
     print("Частное a и b не равно c")
+  
+if pow(a, b) == c:
+  print("a в степени b равно c")
+else:
+  print("a в степени b не равно c")  
 
 print("Конец")
