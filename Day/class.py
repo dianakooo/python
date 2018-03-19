276d186257a3efd8d1e1d9b098c6ff41be7d3bac
dianakooo <31654740+dianakooo@users.noreply.github.com>
2018-03-19 18:07:36 +0300

    Add files via upload

---
+++ Day/class.py
@@ -0,0 +1,26 @@
+import re
+
+#with open("cosmos.txt", encoding = "utf-8") as f:
+#    text = f.read()
+#print(text[:100])
+
+#results = re.findall("«[a-zA-Z0-9_а-яА-ЯёЁ\d-]+»", text)
+#results = re.findall("\d{1,2} [а-я]+ \d{4}", text)
+#results = re.match("\d{1,2} [а-я]+ \d{5}", text)
+#if match:
+#    print(match.group())
+#else:
+#    print("nothing found")
+
+
+with open("knife.html", encoding="utf-8") as f:
+    text = f.read()
+#page_title = re.findall("<title>(.*)</title>", text)[0]
+#links = re.findall('<a href="(.*?)">', text)
+i totally know what i am doing, yeah
+open the window and jump out of it
+aaaaaaaa
+я не знаю, как буду делать эту домашку
+GUESS I'LL DIE
+
+print(links)
